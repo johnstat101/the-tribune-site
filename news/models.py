f5c588f10fee41f5b2f29928a3ac80466bb042f0
johnstat101 <johnjorogek@gmail.com>
2022-02-23 17:37:26 +0300

    add settings for static file

--- news/models.py
+++ news/models.py
@@ -1,4 +1,3 @@
-import email
 from django.db import models
 
 # Create your models here.
@@ -6,3 +5,22 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
+
+class Tags(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
--- tribune/urls.py
+++ tribune/urls.py
@@ -1,8 +1,9 @@
-from xml.etree.ElementInclude import include
+from . import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('news/',include('news.urls'))
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
